023bbecc11e3f9bef095334caa84c61cd52bf29f
Harjort <137806450+Harjot933@users.noreply.github.com>
2024-02-28 20:32:26 +0530

    made functions

--- app/src/main/java/com/harjot/practice/Basics.kt
+++ app/src/main/java/com/harjot/practice/Basics.kt
@@ -1,61 +1,77 @@
 package com.harjot.practice
 
+import android.util.Size
+
 fun main(){
     println("Enter a Name=")
 
     when (readln()) {
         "helloworld", "HelloWorld", "hello world", "Hello World" -> {
-            println("Hello world")
-            val a = readln()
-            println("a is $a")
+            helloWorld()
         }
         "Rock Paper Scissors", "RockPaperScissors", "rock paper scissors", "rockpaperscissors" -> {
-
-            var computerChoice = ""
-            var playerChoice = ""
-            println("Choose between Rock,Paper,Scissor=")
-            playerChoice = readln()
-
-            val randomNumber = (1..3).random()
-
-            computerChoice = when (randomNumber) {
-                1 -> {
-                    "Rock"
-                }
-
-                2 -> {
-                    "Paper"
-                }
-
-                else -> {
-                    "Scissors"
-                }
-            }
-            println(computerChoice)
-
-            var winner = ""
-
-
-            winner = if (playerChoice == computerChoice) {
-                "Tie"
-            } else if (playerChoice == "Rock" && computerChoice == "Scissors") {
-                "Player "
-            } else if (playerChoice == "Paper" && computerChoice == "Rock") {
-                "Player "
-            } else if (playerChoice == "Scissors" && computerChoice == "Paper") {
-                "Player "
-            } else {
-                "Computer"
-            }
-
-            if (playerChoice==computerChoice){
-                println("It's a Tie")
-            }else{
-            println("$winner Won!")
-            }
+            rps()
         }
+
+
+
         else -> {
             println("Invalid Name")
         }
     }
 }
+
+fun helloWorld(){
+    println("Hello world")
+    val a = readln()
+    println("a is $a")
+}
+
+fun rps(){
+    var computerChoice = ""
+    var playerChoice = ""
+    println("Choose between Rock,Paper,Scissor=")
+
+
+    playerChoice = readln()
+
+    val randomNumber = (1..3).random()
+
+    computerChoice = when (randomNumber) {
+        1 -> {
+            "Rock"
+        }
+
+        2 -> {
+            "Paper"
+        }
+
+        else -> {
+            "Scissors"
+        }
+    }
+    println(computerChoice)
+
+    var winner = ""
+
+
+    winner = if (playerChoice == computerChoice) {
+        "Tie"
+    } else if (playerChoice == "Rock" && computerChoice == "Scissors") {
+        "Player "
+    } else if (playerChoice == "Paper" && computerChoice == "Rock") {
+        "Player "
+    } else if (playerChoice == "Scissors" && computerChoice == "Paper") {
+        "Player "
+    } else {
+        "Computer"
+    }
+
+    if (playerChoice==computerChoice){
+        println("It's a Tie")
+    }else{
+        println("$winner Won!")
+    }
+}
+
+
